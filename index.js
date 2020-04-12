0fe77a3ca3cd274b6a03effc5118ed47bd0c9bf9
Farhan Irani <farhanirani007@gmail.com>
2020-04-12 18:02:05 +0530

    autoplay added

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 function onlyPlayOneIn(container) {
-    container.addEventListener("play", function(event) {
+  container.addEventListener("play", function(event) {
     audio_elements = container.getElementsByTagName("audio")
-      for(i=0; i < audio_elements.length; i++) {
-        audio_element = audio_elements[i];
-        if (audio_element !== event.target) {
-          audio_element.pause();
-        }
+    for(i=0; i < audio_elements.length; i++) {
+      audio_element = audio_elements[i];
+      if (audio_element !== event.target) {
+        audio_element.pause();
       }
-    }, true);
-  }
+    }
+  }, true);
+}
+
+function autoPLay(container) {
+  container.addEventListener("ended", function(event) {
+    audio_elements = container.getElementsByTagName("audio")
+    for(i=0; i < audio_elements.length; i++) {
+      audio_element = audio_elements[i];
+      if (audio_element == event.target) {
+        audio_elements[i+1].play();
+      }
+    }
+  }, true);
+}
+
+
   
-  document.addEventListener("DOMContentLoaded", function() {
-    onlyPlayOneIn(document.body);
-  });
-  
+document.addEventListener("DOMContentLoaded", function() {
+  onlyPlayOneIn(document.body);
+  autoPLay(document.body);
+});
